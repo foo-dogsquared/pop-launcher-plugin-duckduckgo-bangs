18ea9f861c2d631e7a5c9a0fb308f45b8927a3c1
Gabriel Arazas <foo.dogsquared@gmail.com>
2021-09-25 18:56:45 +0800

    Change query format completely
    Now that the perceived performance of the plugin is getting better, we
    now have the capacity to change the clunky way of searching with a
    simpler way through prefix. It is easier to use it in this way.
    
    Also, updated the related documentation with a new demo video.

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ mod config;
 mod utils;
 
 use std::collections::HashMap;
-use std::convert::From;
-use std::fmt::{self, Display};
 use std::io;
 
 use pop_launcher::{PluginResponse, PluginSearchResult, Request};
@@ -51,7 +49,7 @@ struct App {
     cache: Vec<(String, String)>,
 
     /// Metadata relating to the user input.
-    search: BangsQuery,
+    search: Vec<String>,
 
     /// The search result where it holds the ID returned from Pop launcher.
     /// The string is assumed to be the trigger word of one of the bangs from the database.
@@ -80,7 +78,7 @@ impl Default for App {
             db,
             cache,
             out: io::stdout(),
-            search: BangsQuery::default(),
+            search: Vec::new(),
             results: HashMap::new(),
         }
     }
@@ -90,9 +88,9 @@ impl App {
     /// Opens the selected bangs and its URL.
     /// Upon activation, it also closes the launcher.
     fn activate(&mut self, _id: u32) {
-        let query = self.search.query.join(" ");
+        let query = self.get_search_query();
         let encoded_query = encode(&query);
-        for bang_trigger in &self.search.bangs {
+        for bang_trigger in self.get_bang_query() {
             if let Some(bang) = self.db.get(bang_trigger) {
                 let url = bang.url.clone().replace(BANGS_PLACEHOLDER, &encoded_query);
                 utils::xdg_open(url);
@@ -108,41 +106,78 @@ impl App {
         if let Some(search) = query.strip_prefix(PLUGIN_PREFIX) {
             // Set the search metadata right after the plugin is enabled.
             // This is just to make input processing easier.
-            self.search = BangsQuery::from(search.to_string());
+            self.search = search.split_whitespace().map(|q| q.to_string()).collect();
 
             // We're just going to base our search from the last bang since it is the most
             // practical way to do so.
+            // It will also do its work when the last part of the query is a bang to not let the
+            // plugin take more than what it needs.
             // We'll figure how to make it smarter by giving responses to recent edits later.
-            let query = self.get_query().to_lowercase();
+            if let Some(query) = self
+                .search
+                .last()
+                .unwrap_or(&String::new())
+                .strip_prefix(BANG_INDICATOR)
+            {
+                // Making the standard output accessible in the closure of the following block.
+                let mut out = &self.out;
 
-            // Making the standard output accessible in the closure of the following block.
-            let mut out = &self.out;
+                // Getting a new hashmap for the results.
+                // Until we can find an elegant way to just use the original map, this will do for now.
+                let mut results = HashMap::new();
+                let mut id = 0;
 
-            // Getting a new hashmap for the results.
-            // Until we can find an elegant way to just use the original map, this will do for now.
-            let mut results = HashMap::new();
-            let mut id = 0;
+                self.cache
+                    .iter()
+                    .filter(|(_trigger, item)| item.contains(query))
+                    .filter_map(|(trigger, _item)| self.db.get(trigger))
+                    .take(SEARCH_RESULT_LIMIT as usize)
+                    .for_each(|bang| {
+                        // This also doubles as a counter.
+                        id += 1;
 
-            self.cache
-                .iter()
-                .filter(|(_trigger, item)| item.contains(&query))
-                .filter_map(|(trigger, _item)| self.db.get(trigger))
-                .take(SEARCH_RESULT_LIMIT as usize)
-                .for_each(|bang| {
+                        utils::send(
+                            &mut out,
+                            PluginResponse::Append(PluginSearchResult {
+                                id: id as u32,
+                                name: bang.name(),
+                                description: bang.description(),
+                                ..Default::default()
+                            }),
+                        );
+
+                        results.insert(id, bang.trigger.clone());
+                    });
+
+                // Send an extra launcher item if there's no search result.
+                // We'll just reuse the `id` variable for this.
+                // Take note we need to make a launcher item to make activation event possible.
+                if id == 0 {
+                    // We send a launcher item since it is required for the activate event.
                     utils::send(
-                        &mut out,
+                        &mut self.out,
                         PluginResponse::Append(PluginSearchResult {
-                            id: id as u32,
-                            name: bang.name(),
-                            description: bang.description(),
+                            id: 1,
+                            name: "Finish".to_string(),
+                            description: "Launch this item to open all your URLs".to_string(),
                             ..Default::default()
                         }),
                     );
+                }
 
-                    results.insert(id, bang.trigger.clone());
-                    id += 1;
-                });
-            self.results = results;
+                self.results = results;
+            } else {
+                // We send a launcher item since it is required for the activate event.
+                utils::send(
+                    &mut self.out,
+                    PluginResponse::Append(PluginSearchResult {
+                        id: 1,
+                        name: "Finish".to_string(),
+                        description: "Launch this item to open all your URLs".to_string(),
+                        ..Default::default()
+                    }),
+                );
+            }
         }
 
         utils::send(&mut self.out, PluginResponse::Finished);
@@ -156,74 +191,32 @@ impl App {
             // If the associated trigger is in the database.
             if let Some(bang) = self.db.get(trigger) {
                 // For the best user experience, we just delete the last element first and update the query.
-                match self.has_query_bang_search() {
-                    true => self.search.query.pop(),
-                    _ => None,
-                };
-
-                self.search.bangs.pop();
-                self.search.bangs.push(bang.trigger.clone());
-                utils::send(&mut self.out, PluginResponse::Fill(self.search.to_string()));
+                self.search.pop();
+                self.search
+                    .push(BANG_INDICATOR.to_string() + &bang.trigger.clone());
+                utils::send(
+                    &mut self.out,
+                    PluginResponse::Fill(format!("{} {}", PLUGIN_PREFIX, &self.search.join(" "))),
+                );
             }
         }
     }
 
-    /// Get the search query to be based.
-    fn get_query(&self) -> String {
-        let ss = String::new();
-        let s = self.search.query.last().unwrap_or(&ss);
-
-        match s.strip_prefix(BANG_INDICATOR) {
-            Some(q) => q.to_string(),
-            None => self.search.bangs.last().unwrap_or(&ss).clone(),
-        }
-    }
-
-    fn has_query_bang_search(&self) -> bool {
+    /// Get the bangs from the search query.
+    fn get_bang_query(&self) -> Vec<&str> {
         self.search
-            .query
-            .last()
-            .unwrap_or(&String::new())
-            .starts_with(BANG_INDICATOR)
+            .iter()
+            .filter_map(|q| q.strip_prefix(BANG_INDICATOR))
+            .collect()
     }
-}
 
-/// Contains the data for the user input.
-/// Since the plugin requires the search query in a certain format, this makes it easier to handle.
-#[derive(Debug, Default)]
-struct BangsQuery {
-    /// An array of triggers to be opened.
-    /// This came from user input as a comma-separated list of triggers (e.g., `g,ddg,yt`).
-    bangs: Vec<String>,
-
-    /// The search query.
-    /// This is assumed it came from the user input.
-    query: Vec<String>,
-}
-
-impl Display for BangsQuery {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{} {} {}",
-            PLUGIN_PREFIX,
-            self.bangs.join(","),
-            self.query.join(" "),
-        )
-    }
-}
-
-impl From<String> for BangsQuery {
-    fn from(s: String) -> Self {
-        let mut s = s.split_whitespace();
-        let bangs = s
-            .next()
-            .unwrap_or("")
-            .split(',')
-            .map(|b| b.to_string())
-            .collect();
-        let query = s.map(|q| q.to_string()).collect();
-
-        Self { bangs, query }
+    /// Get all of the non-bangs from the search query.
+    fn get_search_query(&self) -> String {
+        self.search
+            .iter()
+            .filter(|q| !q.starts_with(BANG_INDICATOR))
+            .map(|q| q.to_string())
+            .collect::<Vec<String>>()
+            .join(" ")
     }
 }
