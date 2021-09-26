80bc4e53b8f6c4e7631aeee43584bf0cf1eb1ded
Gabriel Arazas <foo.dogsquared@gmail.com>
2021-09-26 09:17:20 +0800

    Add automatic download of database when missing
    Though, it needs `curl` for it to work.

--- src/config.rs
+++ src/config.rs
@@ -1,14 +1,20 @@
 use std::collections::HashMap;
-use std::iter::Iterator;
+use std::fs::File;
+use std::io::Write;
 use std::path::PathBuf;
+use std::process::Command;
 
-use pop_launcher::plugin_paths;
+use crate::utils::{find, local_plugin_dir};
+
 use serde::Deserialize;
+
+static DEFAULT_BANGS_DATABASE: &str = "https://duckduckgo.com/bang.js";
 
 /// The bangs database.
 /// It's based from how [Duckduckgo's own database](https://duckduckgo.com/bang.js) is structured.
 pub type Database = HashMap<String, Bang>;
 
+/// A bang object directly based from the Duckduckgo's database.
 #[derive(Debug, Deserialize)]
 pub struct Bang {
     #[serde(alias = "c", default)]
@@ -56,6 +62,9 @@ impl Bang {
 
 /// Loads the database (`db.json`) from the plugin paths.
 /// Please take note it merges the database from each plugin path.
+///
+/// It will also take care of automatically downloading the default database in the local plugin
+/// path if there's no database found in the plugin paths.
 pub fn load() -> Database {
     let mut db = Database::default();
 
@@ -63,11 +72,45 @@ pub fn load() -> Database {
     // I'm not sure if this is ideal so expect this will change in the future.
     // It'll most likely change to override the top-level plugin path (e.g., the user plugins
     // directory over the distribution plugin path).
+    let mut paths: Vec<PathBuf> = find("bangs", "db.json").collect();
+
+    // Download Duckduckgo's bang database when there's no such database anywhere.
+    // We'll download it in the home directory (since that is just the safest location for it).
+    // Specifically at `LOCAL` variable given from the `pop_launcher` crate.
+    // Being synchronous makes it a bit harder to handle this well.
+    //
+    // We also use `curl` from the command line instead of using an HTTP client because I just want
+    // to save some bytes lel.
+    if paths.is_empty() {
+        eprintln!("[bangs] no found database files, downloading the default database");
+        match Command::new("curl")
+            .arg("--silent")
+            .arg(DEFAULT_BANGS_DATABASE)
+            .output()
+        {
+            Ok(process) => {
+                let mut db_path = local_plugin_dir("bangs");
+                db_path.push("db.json");
+
+                if let Ok(mut file) = File::create(&db_path) {
+                    match file.write(&process.stdout) {
+                        Ok(_) => eprintln!("[bangs] default database file successfully downloaded"),
+                        Err(e) => eprintln!("[bangs] not able to write in to file: {}", e),
+                    }
+                }
+
+                paths.push(db_path);
+            }
+            Err(err) => eprintln!("[bangs] default database download failed: {}", err),
+        }
+    }
+
+    // Merge all databases.
     for path in find("bangs", "db.json") {
         let string = match std::fs::read_to_string(&path) {
             Ok(string) => string,
             Err(why) => {
-                eprintln!("failed to read config: {}", why);
+                eprintln!("[bangs] failed to read config: {}", why);
                 continue;
             }
         };
@@ -78,29 +121,9 @@ pub fn load() -> Database {
                     db.insert(bang.trigger.clone(), bang);
                 }
             }
-            Err(why) => eprintln!("failed to deserialize config: {}", why),
+            Err(why) => eprintln!("[bangs] failed to deserialize config: {}", why),
         }
     }
 
     db
-}
-
-/// Find `file` inside of the plugin `name` in each of the plugin paths.
-/// Useful for finding configuration files.
-pub fn find<'a>(name: &'a str, file: &'a str) -> impl Iterator<Item = PathBuf> + 'a {
-    plugin_paths()
-        .filter_map(|path| path.read_dir().ok())
-        .flat_map(move |dir| {
-            dir.filter_map(Result::ok).filter_map(move |entry| {
-                if entry.file_name() == name {
-                    let path = entry.path();
-                    let file_path = path.join(file);
-                    if file_path.exists() {
-                        return Some(file_path);
-                    }
-                }
-
-                None
-            })
-        })
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,12 @@
 use std::ffi::OsStr;
+use std::fs;
 use std::io::{BufRead, Write};
+use std::iter::Iterator;
+use std::path::PathBuf;
 use std::process::Command;
 
-use pop_launcher::PluginResponse;
+use dirs::home_dir;
+use pop_launcher::{plugin_paths, PluginResponse, LOCAL_PLUGINS};
 use serde::Deserialize;
 
 /// Fetch the inputs from a buffer.
@@ -28,4 +32,39 @@ pub fn send(tx: &mut impl Write, response: PluginResponse) {
 /// Open the given file through `xdg-open` command.
 pub fn xdg_open<S: AsRef<OsStr>>(file: S) {
     let _ = Command::new("xdg-open").arg(file).spawn();
+}
+
+/// Find `file` inside of the plugin `name` in each of the plugin paths.
+/// Useful for finding configuration files.
+pub fn find<'a>(name: &'a str, file: &'a str) -> impl Iterator<Item = PathBuf> + 'a {
+    plugin_paths()
+        .filter_map(|path| path.read_dir().ok())
+        .flat_map(move |dir| {
+            dir.filter_map(Result::ok).filter_map(move |entry| {
+                if entry.file_name() == name {
+                    let path = entry.path();
+                    let file_path = path.join(file);
+                    if file_path.exists() {
+                        return Some(file_path);
+                    }
+                }
+
+                None
+            })
+        })
+}
+
+/// Returns the local path of the given plugin as `name`.
+pub fn local_plugin_dir(name: &str) -> PathBuf {
+    let mut path = fs::canonicalize(LOCAL_PLUGINS)
+        .unwrap_or_else(|_| PathBuf::from("~/.local/share/pop-launcher/plugins"));
+
+    if let Ok(p) = path.strip_prefix("~/") {
+        // We'll panic here since it is expected to run it with a home directory.
+        path = home_dir().expect("user does not have home dir").join(p)
+    }
+
+    path.push(name);
+
+    path
 }
